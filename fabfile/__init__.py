65d0e064ff971c8ee72babb7790f0488058960bf
Cornel Nițu <cornel.nitu@eaudeweb.ro>
2017-01-27 16:06:20 +0200

    fixed zeo commands

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -25,68 +25,56 @@ def _with_deploy_env(commands=[], use_sudo=True):
 
 
 def tail():
-    command = 'tail -f /var/log/messages | grep -e www1 -e www2'
+    if env.executable == 'www':
+        command = 'tail -f /var/log/messages | grep -e www1 -e www2'
+    else:
+        command = 'tail -f /var/log/messages | grep -e zeoserver'
     sudo(command)
 
 
 def pull():
-    """
-    Do a git pull.
-    """
     env.forward_agent = True
     _with_deploy_env(['git pull origin master'], use_sudo=False)
 
 
 def stop():
-    """
-    Shutdown the instance and zeo.
-    """
-    command1 = 'scl enable python27 "{}/bin/www1 stop"'.format(env.directory)
-    command2 = 'scl enable python27 "{}/bin/www2 stop"'.format(env.directory)
-    sudo(command1, user=env.deploy_user)
-    sudo(command2, user=env.deploy_user)
+    if env.executable == 'www':
+        cmd = 'scl enable python27 "{}/bin/www1 stop"'.format(env.directory)
+        sudo(cmd, user=env.deploy_user)
+        cmd = 'scl enable python27 "{}/bin/www2 stop"'.format(env.directory)
+        sudo(cmd, user=env.deploy_user)
+    else:
+        cmd = 'scl enable python27 "{}/bin/zeoserver stop"'.format(env.directory)
+        sudo(cmd, user=env.deploy_user)
 
 
 def start():
-    """
-    Start up the instance and zeo.
-    """
-    command1 = 'scl enable python27 "{}/bin/www1 start"'.format(env.directory)
-    command2 = 'scl enable python27 "{}/bin/www2 start"'.format(env.directory)
-    sudo(command1, user=env.deploy_user)
-    sudo(command2, user=env.deploy_user)
+    if env.executable == 'www':
+        cmd = 'scl enable python27 "{}/bin/www1 start"'.format(env.directory)
+        sudo(cmd, user=env.deploy_user)
+        cmd = 'scl enable python27 "{}/bin/www2 start"'.format(env.directory)
+        sudo(cmd, user=env.deploy_user)
+    else:
+        cmd = 'scl enable python27 "{}/bin/zeoserver start"'.format(env.directory)
+        sudo(cmd, user=env.deploy_user)
 
 
 def restart():
-    """
-    Restart just the zope instance, not the zeo.
-    """
-    command1 = 'scl enable python27 "{}/bin/www1 restart"'.format(
-        env.directory)
-    command2 = 'scl enable python27 "{}/bin/www2 restart"'.format(
-        env.directory)
-    sudo(command1, user=env.deploy_user)
-    sudo(command2, user=env.deploy_user)
+    stop()
+    start()
 
 
 def status():
-    """
-    Find out the running status of the server and deploy.
-    """
-
-    # General health of the server.
     run('uptime')
-
-    # Deploy and running status
-    _with_deploy_env(['scl enable python27 "./bin/www1 status"',
+    if env.executable == 'www':
+        _with_deploy_env(['scl enable python27 "./bin/www1 status"',
                       'scl enable python27 "./bin/www2 status"',
                       'git log -1'])
-
+    else:
+        _with_deploy_env(['scl enable python27 "./bin/zeoserver status"',
+                      'git log -1'])
 
 def buildout():
-    """
-    Rerun buildout.
-    """
     command = 'scl enable python27 "{0}/bin/buildout -c {0}/{1}"'.format(
         env.directory, env.buildout_config)
     sudo(command, user=env.deploy_user)
@@ -100,11 +88,3 @@ def deploy():
     stop()
     buildout()
     start()
-
-
-def extra():
-    """ Should normally just contain 'pass'. Useful for
-        testing individual commands before integrating them into
-        another function.
-    """
-    pass
